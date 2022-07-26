77933e2ba1220e4a082ee4b2bfc243df634e2435
James Slinger <jamesxslinger@gmail.com>
2022-07-26 13:23:40 +0100

    Initial commit

---
+++ audio_data.py
@@ -0,0 +1,38 @@
+from pytube import YouTube
+
+
+class AudioStream:
+    def __init__(self):
+        self.track_name = "<Track Name>"
+        self.id = None
+        self.duration = None
+        self.bitrate = None
+        self.filesize = None
+        self.thumbnail = "static/images/music.png"
+        self.source_url = None
+
+    def fetch_streams(self, url):
+        yt_data = YouTube(url)
+        audio_streams = yt_data.streams.filter(only_audio=True)
+        all_streams = []
+        for stream in audio_streams:
+            stream_item = AudioStream()
+            stream_item.source_url = url
+            stream_item.thumbnail = yt_data.thumbnail_url
+            stream_item.track_name = yt_data.title
+            duration_calc = str(yt_data.length / 60).split('.')
+            stream_mins = int(duration_calc[0])
+            stream_secs = int(float('0.' + duration_calc[1])*60)
+            stream_item.duration = f"{stream_mins} mins {stream_secs} secs"
+            stream_item.id = stream.itag
+            stream_item.bitrate = stream.abr
+            stream_item.filesize = f"{round(stream.filesize / 1000 / 1000)}MB"
+            all_streams.append(stream_item)
+        all_streams.sort(key=lambda x: x.filesize, reverse=True)
+        return all_streams
+
+    def download_track(self, url, track_id):
+        yt_data = YouTube(url)
+        dl_track = yt_data.streams.get_by_itag(track_id)
+        print(dl_track.title)
+        return dl_track.download(filename=f"{dl_track.title}.mp3")
---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, send_file, session
+from audio_data import AudioStream
+from pytube import YouTube
+from io import BytesIO
+import datetime as dt
+import os
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    current_year = dt.datetime.now().year
+    return render_template("index.html", year=current_year)
+
+
+@app.route("/results", methods=["POST"])
+def results():
+    current_year = dt.datetime.now().year
+    if request.method == "POST":
+        session['link'] = request.form.get('url')
+    try:
+        url = YouTube(session['link'])
+        url.check_availability()
+        source_url = request.form["url"]
+        track_streams = AudioStream().fetch_streams(source_url)
+        return render_template("results.html", all_streams=track_streams, url=url, year=current_year)
+    except:
+        return render_template("index.html", year=current_year, error="Sorry, that URL didn't work. Please try again.")
+
+
+@app.route("/download", methods=["POST"])
+def download():
+    if request.method == "POST":
+        buffer = BytesIO()
+        session['link'] = request.form.get('url')
+        streams = YouTube(session['link'])
+        track_id = int(request.form.get('track_id'))
+        track = streams.streams.get_by_itag(track_id)
+        track.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(
+            buffer,
+            as_attachment=True,
+            download_name=f"{track.title}.mp3",
+            mimetype="audio/mp3",
+        )
+
+
+if __name__ == "__main__":
+    app.run()
